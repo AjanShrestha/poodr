6abc42f18c6be8de5de903029dd52a7c10eb82a2
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-12 12:08:41 +1100

    feat: Hide Data Structures

--- chapter_2.rb
+++ chapter_2.rb
@@ -101,3 +101,43 @@ end
 def cog
   @cog * (foo? ? bar_adjustment : baz_adjustment)
 end
+
+############## Page 26 ##############
+class ObscuringReferences
+  attr_reader :data
+  def initialize(data)
+    @data = data
+  end
+  
+  def diameters
+    # 0 is rim, 1 is tire
+    data.collect {|cell|
+      cell[0] + (cell[1] * 2)}
+    end
+    # ... many other methods tha index into the array
+  end
+end
+
+############## Page 27 ##############
+# rim and tire sizes (now in millimeters!) in a 2d array
+@data = [[622, 20], [622, 23], [559, 30], [559, 40]]
+
+############## Page 28 ##############
+class RevealingReferences
+  attr_reader: wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect {|wheel|
+      wheel.rim + (wheel.tire * 2)}
+  end
+  # ... now everyone ca send rim/tire to wheel
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect {|cell|
+      Wheel.new(cell[0], cell[1])}
+  end
+end
