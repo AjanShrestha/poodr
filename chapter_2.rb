c98006bcb7a6f931f9ed0ec3289b264ff41457f9
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-12 11:12:28 +1100

    feat: Add Gear Class

--- chapter_2.rb
+++ chapter_2.rb
@@ -8,3 +8,19 @@ chainring = 30
 cog       = 27
 ratio     = chainring / cog.to_f
 puts ratio                        # -> 1.11111111111111
+
+############## Page 19 ##############
+class Gear
+  attr_reader :chainring, :cog
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog       = cog
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
+
+puts Gear.new(52, 11).ratio       # -> 4.72727272727273
+puts Gear.new(30, 27).ratio       # -> 1.11111111111111
