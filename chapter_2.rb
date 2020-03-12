8001dbdcc4b6e0c2fd8c1b4ae6c7fba5901cde79
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-12 11:19:37 +1100

    feat: Add rim and tire attribute and gear_inches method to Gear class

--- chapter_2.rb
+++ chapter_2.rb
@@ -24,3 +24,31 @@ end
 
 puts Gear.new(52, 11).ratio       # -> 4.72727272727273
 puts Gear.new(30, 27).ratio       # -> 1.11111111111111
+
+############## Page 20 ##############
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog       = cog
+    @rim       = rim
+    @tire      = tire
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    # wheel diameter * gear ratio
+    # wheel diameter = rim + twice tire diameter
+    # tire goes around rim twice for diameter
+    ratio * (rim + (tire * 2))
+  end
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+# -> 137.0909090909091
+
+puts Gear.new(52, 11, 24, 1.25).gear_inches
+# -> 125.27272727272728
