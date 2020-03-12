e1a8cd4d3d8d1d0495a72c27aaa4036f5c02552e
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-12 11:12:13 +1100

    feat: Gear Ration calculation

---
+++ chapter_2.rb
@@ -0,0 +1,10 @@
+############## Page 18 ##############
+chainring = 52                    # number of teeth
+cog       = 11
+ratio     = chainring / cog.to_f
+puts ratio                        # -> 4.72727272727273
+
+chainring = 30
+cog       = 27
+ratio     = chainring / cog.to_f
+puts ratio                        # -> 1.11111111111111
