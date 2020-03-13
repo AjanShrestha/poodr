2bd9be338f3880d213d9a08aaf6f4e9c01fd6657
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-13 15:49:04 +1100

    feat: Misapplying Inheritance

--- chapter_6.rb
+++ chapter_6.rb
@@ -251,3 +251,69 @@ puts bike.spares
 # It’s time to abandon this code and revert to the original version 
 # of Bicycle. Perhaps mountain bikes are a specialization of Bicycle; 
 # perhaps this design problem can be solved using inheritance.
+
+
+## Misapplying Inheritance ##
+
+# Under the premise that the journey is more useful than the 
+# destination, and that experi- encing common mistakes by proxy is 
+# less painful than experiencing them in person, this next section 
+# continues to show code that is unworthy of emulation. 
+
+############## Page 115 ##############
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock  = args[:front_shock]
+    @rear_shock   = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+# Jamming the new MountainBike class directly under the existing 
+# Bicycle class was blindly optimistic, and, predictably, running the 
+# code exposes several flaws. Instances of MountainBike have some 
+# behavior that just doesn’t make sense. The following example 
+# shows what happens if you ask a MountainBike for its size and 
+# spares. It reports its size correctly but says that it has skinny 
+# tires and implies that it needs handlebar tape, both of which are 
+# incorrect.
+
+############## Page 115 ##############
+mountain_bike = MountainBike.new(
+  size:          'S',
+  front_shock:  'Manitou',
+  rear_shock:   'Fox'
+)
+
+puts mountain_bike.size  # -> 'S
+
+puts mountain_bike.spares
+# -> {:tire_size    => "23",        <- wrong!
+#     :chain        => "10-speed",
+#     :tape_color   => nil,         <- not applicable
+#     :front_shock  => 'Manitou'
+#     :rear_shock   => "Fox"}
+
+# It comes as no surprise that instances of MountainBike contain a 
+# confusing mishmash of road and mountain bike behavior. The Bicycle 
+# class is a concrete class that was not written to be subclassed. It 
+# combines behavior that is general to all bicycles with behav- ior 
+# that is specific to road bikes. When you slam MountainBike under 
+# Bicycle, you inherit all of this behavior—the general and the 
+# specific, whether it applies or not.
+
+# The Bicycle class contains behavior that is appropriate for both a 
+# peer and a par- ent of MountainBike. Some of the behavior in 
+# Bicycle is correct for MountainBike, some is wrong, and some 
+# doesn’t even apply. As written, Bicycle should not act as the 
+# superclass of MountainBike.
+
+# **
+# Because design is evolutionary, this situation arises all the time. 
+# The problem here started with the names of these classes.
