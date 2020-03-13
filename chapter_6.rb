aa65943065203246c9164d5ded33f22edc252fbe
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-13 15:22:37 +1100

    feat: Embedding Multiple Types

--- chapter_6.rb
+++ chapter_6.rb
@@ -88,3 +88,100 @@ puts bike.spares
 # and tire size. The only differences between road and mountain bikes 
 # are that road bikes need handlebar tape and mountain bikes have 
 # suspension.
+
+### Embedding Multiple Types ###
+
+# When a preexisting concrete class contains most of the behavior you need, it’s tempting to solve this problem by adding code to that class. 
+
+############## Page 110 ##############
+class Bicycle
+  attr_reader :style, :size, :tape_color,
+              :front_shock, :rear_shock
+  
+  def initialize(args)
+    @style       = args[:style]
+    @size        = args[:size]
+    @tape_color  = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
+  end
+
+  # checking 'style' starts down a slippery slope
+  def spares
+    if style == :road
+      {
+        chain:        '10-speed',
+        tire_size:    '23',         # millimeters
+        tape_color:   tape_color
+      }
+    else
+      {
+        chain:        '10-speed',
+        tire_size:    '2.1',        # inches
+        rear_shock:   rear_shock
+      }
+    end
+  end
+end
+
+bike = Bicycle.new(
+  style:        :mountain,
+  size:         'S',
+  front_shock:  'Manitou',
+  rear_shock:   'Fox'
+)
+
+puts bike.spares
+# -> {:tire_size    =>"2.1",
+#     :chain        =>"10-speed",
+#     :rear_shock   =>"Fox"}
+
+
+# This example is simply a detour that illustrates an antipattern, 
+# that is, a common pattern that appears to be beneficial but is 
+# actually detrimental, and for which there is a well-known 
+# alternative.
+
+
+# This code makes decisions about spare parts based on the value held 
+# in style; structuring the code this way has many negative 
+# consequences. If you add a new style you must change the if 
+# statement. If you write careless code where the last option is the 
+# default (as does the code above) an unexpected style will do 
+# something but perhaps not what you expect. Also, the spares method 
+# started out containing embedded default strings, some of these 
+# strings are now duplicated on each side of the if statement.
+
+# Bicycle has an implied public interface that includes spares, size, 
+# and all the individual parts. The size method still works, spares 
+# generally works, but the parts methods are now unreliable. It’s 
+# impossible to predict, for any specific instance of Bicycle, 
+# whether a specific part has been initialized. Objects holding onto 
+# an instance of Bicycle may, for example, be tempted to check style 
+# before sending it tape_color or rear_shock.
+# The code wasn’t great to begin with; this change did nothing to 
+# improve it.
+# The initial Bicycle class was imperfect but its imperfections were 
+# hidden—encapsulated within the class. These new flaws have broader 
+# consequences. Bicycle now has more than one responsibility, 
+# contains things that might change for different reasons, and cannot 
+# be reused as is.
+
+# This code contains an if statement that checks an attribute that 
+# holds the category of self to determine what message to send to s
+# elf. This should bring back memories of a pattern discussed in the 
+# previous chapter on duck typing, where you saw an if statement that 
+# checked the class of an object to determine what message to send to 
+# that object.
+# In both of these patterns an object decides what message to send 
+# based on a category of the receiver. You can think of the class of 
+# an object as merely a specific case of an attribute that holds the 
+# category of self ; considered this way, these patterns are the 
+# same. In each case if the sender could talk it would be saying “I 
+# know who you are and because of that I know what you do.” This 
+# knowledge is a dependency that raises the cost of change.
+
+# Be on the lookout for this pattern. While sometimes innocent and 
+# occasionally defensible, its presence might be exposing a costly 
+# flaw in your design. Here the pattern indicates a missing subtype, 
+# better known as a subclass.
