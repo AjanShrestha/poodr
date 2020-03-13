0bf397cf36bedbe618b28f497ba5666ed82e601b
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-13 11:17:13 +1100

    docs: Reducing Costs with Duck Typing

---
+++ chapter_5.rb
@@ -0,0 +1,17 @@
+# Reducing Costs with Duck Typing
+
+# The purpose of object-oriented design is to reduce the cost of 
+# change. Now that you know messages are at the design center of your 
+# application, and now that you are committed to the construction of 
+# rigorously defined public interfaces, you can combine these two 
+# ideas into a powerful design technique that further reduces your 
+# costs.
+# This technique is known as duck typing. Duck types are public 
+# interfaces that are not tied to any specific class. These 
+# across-class interfaces add enormous flexibility to your 
+# application by replacing costly dependencies on class with more 
+# forgiving dependencies on messages.
+# Duck typed objects are chameleons that are defined more by their 
+# behavior than by their class. This is how the technique gets its 
+# name; if an object quacks like a duck and walks like a duck, then 
+# its class is immaterial, it’s a duck.
