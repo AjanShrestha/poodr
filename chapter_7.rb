b4a4c757aa0d4aaa33e92ad939835daa8a06ceda
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-15 09:16:41 +1100

    docs: Sharing Role Behavior with Modules

---
+++ chapter_7.rb
@@ -0,0 +1,18 @@
+# Sharing Role Behavior with Modules #
+
+# What will happen when FastFeet develops a need for recumbent mountain bikes?
+# If the solution to this new design problem feels elusive, that’s 
+# perfectly under- standable. Creation of a recumbent mountain bike 
+# subclass requires combining the qualities of two existing 
+# subclasses, something that inheritance cannot readily accommodate. 
+# Even more distressing is the fact that this failure illustrates 
+# just one of several ways in which inheritance can go wrong.
+
+# **
+# To reap benefits from using inheritance you must understand not 
+# only how to write inheritable code but also when it makes sense to 
+# do so. Use of classical inheritance is always optional; every 
+# problem that it solves can be solved another way. Because no design 
+# technique is free, creating the most cost-effective application 
+# requires making informed tradeoffs between the relative costs and 
+# likely benefits of alternatives.
