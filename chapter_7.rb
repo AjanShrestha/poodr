7b2c879a638614c23d8dffba745413a930cc7186
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-15 09:28:06 +1100

    docs: Finding Roles

--- chapter_7.rb
+++ chapter_7.rb
@@ -32,3 +32,62 @@
 # they exist nonetheless. Using a role creates dependencies among the 
 # objects involved and these dependencies introduce risks that you 
 # must take into account when deciding among design options.
+
+### Finding Roles ###
+
+# The Preparer duck type from Chapter 5, Reducing Costs with Duck 
+# Typing, is a role. Objects that implement Preparer’s interface play 
+# this role. Mechanic, TripCoordinator, and Driver each implement 
+# prepare_trip; therefore, other objects can interact with them as if 
+# they are Preparers without concern for their underlying class.
+
+# The existence of a Preparer role suggests that there’s also a 
+# parallel Preparable role (these things often come in pairs). The 
+# Trip class acts as a Preparable in the Chapter 5 example; it 
+# implements the Prepareable interface. This interface includes all 
+# of the messages that any Preparer might expect to send to a 
+# Preparable, that is, the methods bicycles, customers, and vehicle.
+# The Preparable role is not terribly obvious because Trip is its 
+# only player but it’s important to recognize that it exists.
+
+# Although the Preparer role has multiple players, it is so simple 
+# that it is entirely defined by its interface. To play this role all 
+# an object need do is implement its own personal version of 
+# prepare_trip. Objects that act as Preparers have only this 
+# interface in common. They share the method signature but no other 
+# code.
+
+# Preparer and Preparable are perfectly legitimate duck types. It’s 
+# far more common, however, to discover more sophisticated roles, 
+# ones where the role requires not only specific message signatures, 
+# but also specific behavior. When a role needs shared behavior 
+# you’re faced with the problem of organizing the shared code. Ideally
+# this code would be defined in a single place but be usable by any 
+# object that wished to act as the duck type and play the role.
+
+# **
+# Many object-oriented languages provide a way to define a named 
+# group of methods that are independent of class and can be mixed in 
+# to any object. In Ruby, these mix-ins are called modules. Methods 
+# can be defined in a module and then the module can be added to any 
+# object. Modules thus provide a perfect way to allow objects of 
+# different classes to play a common role using a single set of code.
+# When an object includes a module, the methods defined therein 
+# become available via automatic delegation. If this sounds like 
+# classical inheritance, it also looks like it, at least from the 
+# point of view of the including object. From that object’s point of 
+# view, messages arrive, it doesn’t understand them, they get 
+# automatically routed somewhere else, the correct method 
+# implementation is magically found, it is executed, and the response 
+# is returned.
+# Once you start putting code into modules and adding modules to 
+# objects, you expand the set of messages to which an object can 
+# respond and enter a new realm of design complexity. An object that 
+# directly implements few methods might still have a very large 
+# response set. The total set of messages to which an object can 
+# respond includes
+# • Those it implements
+# • Those implemented in all objects above it in the hierarchy
+# • Those implemented in any module that has been added to it
+# • Those implemented in all modules added to any object above it in 
+#   the hierarchy
