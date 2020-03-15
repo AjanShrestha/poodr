909020d49f5a199392d8f3da240b4c8b24ed9e46
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-16 10:36:38 +1100

    feat: Creating a Parts Hierarchy

--- chatper_8.rb
+++ chatper_8.rb
@@ -73,3 +73,125 @@ end
 
 # Bicycle is now responsible for three things: knowing its size, 
 # holding onto its Parts, and answering its spares.
+
+### Creating a Parts Hierarchy ###
+
+# That was easy, but only because there wasn’t much bicycle related 
+# behavior in the Bicycle class to begin with; most of the code in 
+# Bicycle dealt with parts. You still need the parts behavior that 
+# you just removed from Bicycle, and the simplest way to get this 
+# code working again is to simply fling that code into a new 
+# hierarchy of Parts, as shown below.
+
+############## Page 165 ##############
+class Parts
+  attr_reader :chain, :tire_size
+
+  def initialize(args={})
+    @chain          = args[:chain]      || default_chain
+    @tire_size      = args[:tire_size]  || default_tire_size
+    post_initialize(args)
+  end
+
+  def spares
+    {
+      tire_size: tire_size,
+      chain: chain
+    }.merge(local_spares)
+  end
+
+  def default_tire_size
+    raise NotImplementedError
+  end
+
+  # subclasses may override
+  def post_initialize(args)
+    nil
+  end
+
+  def local_spares
+    {}
+  end
+
+  def default_chain
+    '10-speed'
+  end
+end
+
+class RoadBikeParts < Parts
+  attr_reader :tape_color
+
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+  end
+
+  def local_spares
+    {tape_color: tape_color}
+  end
+
+  def default_tire_size
+    '23'
+  end
+end
+
+class MountainBikeParts < Parts
+  attr_reader :front_shock, :rear_shock
+
+  def post_initialize(args)
+    @front_shock  = args[:front_shock]
+    @rear_shock   = args[:rear_shock]
+  end
+
+  def local_spares
+    {rear_shock: rear_shock}
+  end
+
+  def default_tire_size
+    '2.1'
+  end
+end
+
+# This code is a near exact copy of the Bicycle hierarchy from 
+# Chapter 6; the differences are that the classes have been renamed 
+# and the size variable has been removed.
+
+# The class diagram in Figure 8.3 illustrates this transition. There 
+# is now an abstract Parts class. Bicycle is composed of Parts. Parts 
+# has two subclasses, RoadBikeParts and MountainBikeParts.
+
+# After this refactoring, everything still works. As you can see 
+# below, regardless of whether it has RoadBikeParts or 
+# MountainBikeParts, a bicycle can still correctly answer its size 
+# and spares.
+
+############## Page 167 ##############
+road_bike =
+  Bicycle.new(
+    size:  'L',
+    parts: RoadBikeParts.new(tape_color: 'red'))
+
+puts road_bike.size    # -> 'L'
+
+puts road_bike.spares
+# -> {:tire_size=>"23",
+#     :chain=>"10-speed",
+#     :tape_color=>"red"}
+
+mountain_bike =
+  Bicycle.new(
+    size:  'L',
+    parts: MountainBikeParts.new(rear_shock: 'Fox'))
+
+puts mountain_bike.size   # -> 'L'
+
+puts mountain_bike.spares
+# -> {:tire_size=>"2.1",
+#     :chain=>"10-speed",
+#     :rear_shock=>"Fox"}
+
+# **
+# This wasn’t a big change and it isn’t much of an improvement. 
+# However, this refactoring did reveal one useful thing; it made it 
+# blindingly obvious just how little Bicycle specific code there was 
+# to begin with. Most of the code above deals with individual parts; 
+# the Parts hierarchy now cries out for another refactoring.
