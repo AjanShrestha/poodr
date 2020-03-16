2206838b4d81178a98f9727bd8df8c6b1205d8ce
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-16 22:25:30 +1100

    docs: Testing Duck Types

---
+++ chapter_9e.rb
@@ -0,0 +1,8 @@
+## Testing Duck Types ##
+# The Testing Incoming Messages section in this chapter wandered into 
+# the territory of testing roles, but while it introduced the issue, 
+# it did not provide a satisfactory resolution. It’s time to return 
+# to that topic and examine how to test duck types. This section 
+# shows how to create tests that role players can share and then 
+# returns to the original problem and uses shareable tests to prevent 
+# test doubles from becoming obsolete.
