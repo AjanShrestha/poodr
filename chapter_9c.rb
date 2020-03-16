13683dae98ff6a8d38c71a53b6b2b7f10b1215e0
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-16 22:12:46 +1100

    docs: Choosing to Test a Private Method

--- chapter_9c.rb
+++ chapter_9c.rb
@@ -56,3 +56,51 @@
 # magically become more reliable just because they got moved. It is 
 # costly to couple to unstable methods—regardless of whether they are 
 # portrayed as public or private.
+
+### Choosing to Test a Private Method ###
+# Times of great uncertainly call for drastic measures. It is 
+# therefore occasionally defensible to fling a bit of smelly code 
+# into place and hide the mess until better information arrives. 
+# Hiding messes is easily done; just wrap the offending code in a 
+# private method.
+
+# If you create a mess and never fix it your costs will eventually go 
+# up, but in the short term, for the right problem, having enough 
+# confidence to write embarrassing code can save money. When your 
+# intention is to defer a design decision, do the simplest thing that 
+# solves today’s problem. Isolate the code behind the best interface 
+# you can conceive and hunker down and wait for more information.
+
+# Applying this strategy can result in private methods that are 
+# wildly unstable. Once you’ve made this leap it’s reasonable to 
+# consider compounding your sins by testing these unstable methods. 
+# The application code is ugly and will undergo frequent change; the 
+# risk of breaking something is ever-present. These tests are costly 
+# and will likely be forced to change in lock-step with the 
+# underlying code, but every other option for keeping things running 
+# may be more expensive.
+
+# These tests of private methods aren’t necessary in order to know 
+# that a change broke something, the public interface tests still 
+# serve that purpose admirably. Tests of private methods produce 
+# error messages that directly pinpoint the failing parts of private 
+# code. These more specific errors are tight couplings that increase 
+# maintenance costs, but they make it easier to understand the 
+# effects of changes and so they take some of the pain out of 
+# refactoring complex private code.
+
+# Reducing the barriers to refactoring is important, because refactor 
+# you will. That’s the whole point. The mess is temporary, you intend 
+# to refactor out of it. As more design information arrives, these 
+# private methods will improve. Once the fog clears and a design 
+# reveals itself, the methods will become more stable. As stability 
+# improves, the cost of maintaining and the need for tests will go 
+# down. Eventually it will be possible to extract the private methods 
+# into a separate class and safely expose them to the world.
+
+# **
+# The rules-of-thumb for testing private methods are thus: 
+#   Never write them, and if you do, never ever test them, unless of 
+#     course it makes sense to do so. Therefore, be biased against 
+#     writing these tests but do not fear to do so if this would 
+#     improve your lot.
