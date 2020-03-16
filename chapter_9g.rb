1f05a5bd406301e9f437bfd4691757b5949867dd
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-17 07:48:08 +1100

    docs: Testing Inherited Code

---
+++ chapter_9g.rb
@@ -0,0 +1,10 @@
+## Testing Inherited Code ##
+
+# You’ve finally arrived at the last challenge, testing inherited 
+# code. This section is much like the previous ones in that it 
+# recapitulates a previously seen example and then proceeds to test 
+# it. The example used here is the final Bicycle hierarchy from 
+# Chapter 6, Acquiring Behavior Through Inheritance. Even though that 
+# hierarchy eventually proved unsuitable for inheritance, the 
+# underlying code is fine and serves admirably as a basis for these 
+# tests.
