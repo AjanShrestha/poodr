eb15eafd9e84d3e2d0d698f71c5d19d654adada3
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-16 22:14:24 +1100

    docs: Testing Outgoing Messages

---
+++ chapter_9d.rb
@@ -0,0 +1,5 @@
+## Testing Outgoing Messages ##
+# Outgoing messages, as you know from the “What to Test” section, are 
+# either queries or commands. Query messages matter only to the 
+# object that sends them, while command messages have effects that 
+# are visible to other objects in your application.
